f9abdc37289d142808bdc5b9a823318086b933f3
Amalia2016 <amaliagr2001@yahoo.com>
2017-03-22 16:43:56 -0600

    New files added

--- compute.py
+++ compute.py
@@ -14,7 +14,7 @@ def compute(ticker, cols):
     # https://www.quandl.com/api/v3/datasets/EOD/[ticker].json?api_key=byL6HFiAJCnKKXttWCW7&collapse=monthly&start_date=1990-01-01
     quandl.ApiConfig.api_key = 'byL6HFiAJCnKKXttWCW7'
     data = quandl.get_table('WIKI/PRICES', ticker = ticker)
-    cols = ['open', 'close']
+#    cols = ['open', 'close']
     df = data.filter(items = cols)
     s = df.mean()
     return s
